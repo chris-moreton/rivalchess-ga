dbefd1fdffff0beba39041f622486e5bbba1c487
Chris Moreton <chris@netsensia.com>
2020-08-03 14:29:04 +0100

    Added script to extract values to CSV

---
+++ src/main/java/com/netsensia/rivalchess/eve/Extract.kt
@@ -0,0 +1,32 @@
+package com.netsensia.rivalchess.eve
+
+import java.io.File
+
+fun main(args: Array<String>) {
+    val data = mutableListOf(
+            mutableListOf("Pawn"),
+            mutableListOf("Knight"),
+            mutableListOf("Bishop"),
+            mutableListOf("Rook"),
+            mutableListOf("Queen"))
+
+    var previousLine: String = ""
+    File("log/ga 1596379423071.txt").forEachLine { line ->
+        if (previousLine.contains("Average")) {
+            var index = 0
+            line.split(" ").forEach { part ->
+                if (part.trim() != "") data[index++].add(part)
+            }
+        }
+
+        previousLine = line
+    }
+
+    val outFile = File("out.csv")
+    outFile.writeText("Generation,")
+    outFile.appendText((0 until data[0].size - 1).toList().joinToString { it.toString() }.toString() + "\n")
+    (0 until 5).forEach {
+        outFile.appendText(data[it].joinToString { it } + "\n")
+    }
+
+}
